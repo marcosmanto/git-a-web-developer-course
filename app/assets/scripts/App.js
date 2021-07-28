de635b7e3b05fbe1d3e0445a0386a3647d738a66
Marcos Mantovani <mantovani.marcos@gmail.com>
2021-07-28 19:40:23 -0300

    section 6 (configured wepack-dev-server) was completed

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,9 @@
 import '../styles/styles.css'
 
+if(module.hot) {
+  module.hot.accept()
+}
+
 if(navigator.userAgent.match(/chrome|opera/i))
 {
   document.body.style.textShadow='rgba(0,0,0,0.31) 0px 0px 1px';

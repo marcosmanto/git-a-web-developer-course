99c59ba9c1f4ec87b1a4766ab515edfb81db8fdb
Marcos Mantovani <mantovani.marcos@gmail.com>
2021-08-03 23:59:52 -0300

    First optimization to reveal item js module (added lodash throttle)

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -1,15 +1,22 @@
+const {throttle} = require('lodash')
+//import throttle from 'lodash/throttle'
 class RevealOnScroll {
   constructor() {
     this.itensToReveal = document.querySelectorAll('.featured-item')
     this.hideInitially()
+    this.scrollThrottle = throttle(this.calcCaller, 200).bind(this)
     this.events()
   }
 
   events() {
-    window.addEventListener('scroll', () => {
-      this.itensToReveal.forEach( el => {
+    window.addEventListener('scroll', this.scrollThrottle)
+  }
+
+  calcCaller() {
+    this.itensToReveal.forEach( el => {
+      if (!el.isRevealed) {
         this.calculateIfScrolledTo(el)
-      })
+      }
     })
   }
 
@@ -17,11 +24,21 @@ class RevealOnScroll {
     const scrollPercent = (el.getBoundingClientRect().top / window.innerHeight) * 100
     if (scrollPercent < 75) {
       el.classList.add('reveal-item--is-visible')
+      el.isRevealed = true
+      if(el.isLastItem) {
+        window.removeEventListener('scroll', this.scrollThrottle)
+      }
     }
   }
 
   hideInitially() {
-    this.itensToReveal.forEach( el => el.classList.add('reveal-item'))
+    this.itensToReveal.forEach( el => {
+      // mark all elements as not revealead at startup
+      el.isRevealed = false
+      el.classList.add('reveal-item')
+    })
+    // mark last reveal item that appears in the DOM
+    this.itensToReveal[this.itensToReveal.length - 1].isLastItem = true
   }
 }
 

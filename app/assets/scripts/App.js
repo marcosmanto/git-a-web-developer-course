7c2adf80d4a11b7dda3859cbab7f560325c0839a
Marcos Mantovani <mantovani.marcos@gmail.com>
2021-08-06 22:46:01 -0300

    coupled module Modal.js/ User will download module even if not used

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,7 +2,7 @@ import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickHeader from './modules/StickyHeader';
-
+import Modal from './modules/Modal';
 
 if(module.hot) {
   module.hot.accept()
@@ -17,3 +17,5 @@ new RevealOnScroll(document.querySelectorAll('.featured-item'), 75)
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 60)
 const stickyHeader = new StickHeader()
 const mobileMenu = new MobileMenu()
+
+const modal = new Modal

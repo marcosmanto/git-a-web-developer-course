4ee7f31ffd92956a1c4b6205d611e3510b82b60f
Marcos Mantovani <mantovani.marcos@gmail.com>
2021-08-08 13:05:19 -0300

    1. Setup build process.Webpack config adjusted to accept build and dev speccific actions

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const currentTask = process.env.npm_lifecycle_event
+
 const path = require('path')
 
 const postCSSPlugins = [
@@ -8,20 +10,8 @@ const postCSSPlugins = [
   require('autoprefixer'),
 ]
 
-module.exports = {
+let config = {
   entry: './app/assets/scripts/App.js',
-  output: {
-    filename: 'bundled.js',
-    path: path.resolve(__dirname, 'app')
-  },
-  devServer: {
-    before: (app, server) => server._watch('./app/**/*.html'),
-    contentBase: path.join(__dirname, 'app'),
-    hot: true,
-    port: 3000,
-    host: '0.0.0.0'
-  },
-  mode: 'development',
   module: {
     rules: [
       { test: /\.css$/i,
@@ -38,3 +28,28 @@ module.exports = {
     ]
   }
 }
+
+if (currentTask === 'dev') {
+  config.output = {
+    filename: 'bundled.js',
+    path: path.resolve(__dirname, 'app')
+  }
+  config.devServer = {
+    before: (app, server) => server._watch('./app/**/*.html'),
+    contentBase: path.join(__dirname, 'app'),
+    hot: true,
+    port: 3000,
+    host: '0.0.0.0'
+  }
+  config.mode = 'development'
+}
+
+if (currentTask === 'build') {
+  config.output = {
+    filename: 'bundled.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+  config.mode = 'production'
+}
+
+module.exports = config

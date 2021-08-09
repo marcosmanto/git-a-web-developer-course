84ce6f5655224f134a4a205bba828b4d9194b8ff
Marcos Mantovani <mantovani.marcos@gmail.com>
2021-08-09 15:10:03 -0300

    Added cloud backed function to control access to restrict area

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,6 +2,7 @@ import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickHeader from './modules/StickyHeader';
+import ClientArea from './modules/ClientArea';
 import 'lazysizes'
 
 if(module.hot) {
@@ -17,6 +18,7 @@ new RevealOnScroll(document.querySelectorAll('.featured-item'), 75)
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 60)
 new StickHeader()
 new MobileMenu()
+new ClientArea()
 
 let modal
 document.querySelectorAll('.open-modal').forEach( el => {
